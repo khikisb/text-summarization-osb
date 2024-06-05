d01025aaafcb59a0ca258c3c2959b7994a70be46
khikisb <89850965+khikisb@users.noreply.github.com>
2024-06-05 23:28:53 +0700

    Update app.py

--- app.py
+++ app.py
@@ -77,13 +77,8 @@ def ringkas_teks(teks_artikel, top_n=3):
     graph_kemiripan_kalimat = nx.from_numpy_array(matriks_kemiripan_kalimat)
     skor = nx.pagerank(graph_kemiripan_kalimat)
     kalimat_terurut = sorted(((skor[i], s) for i, s in enumerate(kalimat)), reverse=True)
-    
-    try:
-        for i in range(min(top_n, len(kalimat_terurut))):  # Pastikan tidak melebihi panjang kalimat_terurut
-            teks_ringkasan.append(" ".join(kalimat_terurut[i][1]))
-    except IndexError:
-        st.error("Tidak ada kalimat yang dihasilkan dalam proses ringkasan.")
-
+    for i in range(top_n):
+        teks_ringkasan.append(" ".join(kalimat_terurut[i][1]))
     ringkasan = ". ".join(teks_ringkasan)
     buat_word_cloud(ringkasan)
     return ringkasan, graph_kemiripan_kalimat
@@ -107,27 +102,6 @@ with tab1:
     page = st.number_input("Halaman", min_value=1, max_value=total_pages, step=1, value=1)
     start_idx = (page - 1) * per_page
     end_idx = min(start_idx + per_page, len(df))
-
-    # Tampilkan tabel dengan tambahan kolom "Ringkas Berita" dan "Jumlah Kalimat" di setiap baris data
-    for index, row in df.iloc[start_idx:end_idx].iterrows():
-        st.write(f"Penulis: {row['penulis']}")
-        st.write(f"Isi Berita: {row['isi-berita']}")
-        
-        with st.form(key=f"ringkasan_form_{index}"):
-            col1, col2 = st.columns([1, 1])
-            with col1:
-                ringkas_button = st.form_submit_button(label="Ringkas Berita")
-            with col2:
-                kalimat_input = st.number_input(label="Jumlah Kalimat", min_value=1, max_value=10, step=1, value=3)
-        
-        if ringkas_button:
-            ringkasan, _ = ringkas_teks(row["isi-berita"], top_n=kalimat_input)
-            st.subheader("Ringkasan Berita")
-            st.write(ringkasan)
-            st.markdown("---")  # Garis pemisah antar berita
-
-    # Menampilkan tabel berita lengkap
-    st.subheader("Berita Lengkap")
     st.write(df.iloc[start_idx:end_idx])
 
 with tab2:
